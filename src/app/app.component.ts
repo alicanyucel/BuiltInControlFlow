de7206190c62f23ed04083d4df776fc1389f286f
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-12-01 20:39:52 +0300

    BuiltInControlFlow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+ @if(show)
+ {
+  <h1>Show</h1>
+ }
+ @else
+ {
+ <h1>not show</h1>
+ }
+ @switch(role)
+ {
+  @case('user'){
+<h1>User Role</h1>
+  }
+   @default{
+    <h1>Rolunuz bulunamadı</h1
+   }
+ }
+ <ul>
+ @for(todo of todos;track todo)
+ {
+  <li>
+  {{index}}={{todo}}
+  <span *ngIf="$first>İlk kayıt</span>
+  <span *ngIf="$last>Son kayıt</span>
+  </li>
+ }
+ @empty{
+  <h1>Listede hiçbir kayıt bulunamadi</h1>
+ }
+ </ul>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'builtin';
+  show: boolean = false;
+  todos: string[] = ["e1,e2,e3"];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
